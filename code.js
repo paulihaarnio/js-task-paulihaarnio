8aef3c09b11df0ed853e05e6624d7bca630d2c10
paulihaarnio <pauli.haarnio@gmail.com>
2021-12-15 16:24:55 +0200

    icon and font

--- code.js
+++ code.js
@@ -25,23 +25,40 @@ function getWeather() {
 	 if (this.readyState == 4 && this.status == 200) {
          let weather=JSON.parse(this.response)
             document.querySelector("span").innerHTML="The weather of "+weather.location.name+", the tempature is "+weather.current.temp_c+" celcius and the wind is "+Math.floor(weather.current.wind_kph*0.28)+" meters per second"
-	        
+	        document.getElementById("image").setAttribute('src', weather.current.condition.icon)
 	     
 	 }
     };
+    if(document.getElementById("oulu").selected==true){
     xhttp.open("GET", "http://api.weatherapi.com/v1/current.json?key=bc44c2be4a8948448c882920211512&q=Oulu&aqi=no", true);
-    xhttp.send();
+    xhttp.send();}
+    else if(document.getElementById("helsinki").selected==true){
+    xhttp.open("GET", "http://api.weatherapi.com/v1/current.json?key=bc44c2be4a8948448c882920211512&q=Helsinki&aqi=no", true);
+    xhttp.send();}
+    else if(document.getElementById("tampere").selected==true){
+    xhttp.open("GET", "http://api.weatherapi.com/v1/current.json?key=bc44c2be4a8948448c882920211512&q=Tampere&aqi=no", true);
+    xhttp.send();}
+    else if(document.getElementById("lahti").selected==true){
+     xhttp.open("GET", "http://api.weatherapi.com/v1/current.json?key=bc44c2be4a8948448c882920211512&q=Lahti&aqi=no", true);
+     xhttp.send();}
+    else if(document.getElementById("turku").selected==true){
+     xhttp.open("GET", "http://api.weatherapi.com/v1/current.json?key=bc44c2be4a8948448c882920211512q=Turku&aqi=no", true);
+     xhttp.send();}
+    else if(document.getElementById("rovaniemi").selected==true){
+     xhttp.open("GET", "http://api.weatherapi.com/v1/current.json?key=bc44c2be4a8948448c882920211512&q=Rovaniemi&aqi=no", true);
+     xhttp.send();}
 }
 
 function tyhjennä(){
-    document.querySelector("span").innerHTML=""
-    document.getElementById("joke").innerHTML="vitsi tulee tähän"
+    document.querySelector("span").innerHTML="The weather comes here"
+    document.getElementById("joke").innerHTML="The joke comes here"
+    document.getElementById("image").setAttribute('src', "")
 }
 
-document.getElementById("print_weather").addEventListener("click", getWeather)
+document.querySelector("button").addEventListener("click", getWeather)
 
 
-document.querySelector("button").addEventListener("click", get_joke_of_the_day)
+document.getElementById("printjoke").addEventListener("click", get_joke_of_the_day)
 
 document.getElementById("tyhjennys").addEventListener("click", tyhjennä )
 
